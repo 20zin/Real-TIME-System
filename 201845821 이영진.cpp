7cadb4cbc71dd28edb863dd05e29b8accd5e016c
이영진 <117518123+20zin@users.noreply.github.com>
2023-05-30 22:47:59 +0900

    Add files via upload

---
+++ 201845821 이영진.cpp
@@ -0,0 +1,15 @@
+#include  <cpu.h>
+#include  <lib_mem.h>
+#include  <os.h>
+#include <conio.h>
+#include  <time.h>
+#include  "app_cfg.h"
+
+#define         OS_TIME_DELAY
+#define         LOOP         250
+
+#define         TASK_STK_SIZE    256      /* Size of each task's stacks (# of OS_STK)      */
+#define         MSG_QUEUE_SIZE   20
+
+OS_STK          TaskStartStk[TASK_STK_SIZE];   /*스택영역*/
+OS_STK          Task1Stk[TASK_STK_SIZE];       /* Task #1    task stack*/
